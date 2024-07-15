6decb959b5900a8b9ddeff4e357a0058a1e66832
yashlondhe90960 <yashlondhe9151@gmail.com>
2024-07-16 02:09:02 +0530

    Added StackNavigator

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,14 +4,25 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import HomeScreen from './screens/HomeScreen'
 import PickupScreen from './screens/PickUpScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import OrderScreen from './screens/OrderScreen'
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Register" component={RegisterScreen} options={{headerShown:false}}/>
         <Stack.Screen name="PickUp" component={PickupScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Cart" component={CartScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Order" component={OrderScreen} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
